55d0f47a3d9beceb46da63a3630e2db83a900a5d
Tilak Parajuli <128390170+parajulitilak@users.noreply.github.com>
2023-03-30 17:40:04 +0545

    Add files via upload
    python enhancement proposal (pep) guidelines implementation

---
+++ PEP_guidelines.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+
+def my_function():
+    my_array=["one", "Two",
+              "Three", "Four",
+              "Five", "Six",
+              "Seven"," Eight"]
+    print("My name is Tilak Parajuli")
+
+
+def my_second_function():
+    print("HELLO HUMANS")
+
+
+my_list=[1, 2, 3,
+         4, 5, 6,
+         7, 8, 9]
+
+
+print("Hello", "Tilak")
+
+x=3*4 + 5*8
+
+check=True
+
+if check is True:
+    print("TRUE STATEMENT")
