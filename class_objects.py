d370b3e86c6bc3ef5b9b48ead89d7c46dde930af
Tilak Parajuli <128390170+parajulitilak@users.noreply.github.com>
2023-03-31 19:44:38 +0545

    Add files via upload

---
+++ class_objects.py
@@ -0,0 +1,16 @@
+import random
+
+
+class enemy_atk:
+    enemy_high = 80
+    enemy_low = 60
+
+    def get_atk(self):
+        print(self.enemy_low)
+
+
+enemy1 = enemy_atk()
+enemy1.get_atk()
+
+enemy2 = enemy_atk()
+enemy2.get_atk()
